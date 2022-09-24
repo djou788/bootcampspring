bcd6c5b6cfc110babb441b1938f4dec63eb7efe8
djou788 <98538837+djou788@users.noreply.github.com>
2022-09-24 17:09:21 +0100

    passage de data vers la vue

--- src/main/java/com/edinfo/shipping/controllers/HomeController.java
+++ src/main/java/com/edinfo/shipping/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.edinfo.shipping.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,8 +13,12 @@ public class HomeController {
 	}
 	
 	@RequestMapping ("/affichage")
-	public String affichage()
+	public String affichage(Model model)
 	{
+		String formation="Fulstack";
+		System.out.println("Méthode info");
+		model.addAttribute("workshop",formation);
+		
 	return "home/affichage";
 	}
 }
